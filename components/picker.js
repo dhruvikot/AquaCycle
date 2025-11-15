3adc85783beb9c315b4cf3b48b7a76e63ddab58d
Dhruv Savla <dsavla0405@gmail.com>
2025-11-14 16:27:11 -0800

    picker

--- components/picker.js
+++ components/picker.js
@@ -1,172 +1,189 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet,Button,TextInput } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import calls from '../services/calls';
 
 const Contact = ({ clientId }) => {
-    const [clientContactInfo, setClientContactInfo] = useState(null);
-    const [isVisible, setIsVisible] = useState(false);
+  const [clientContactInfo, setClientContactInfo] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const fetchClientContactInfo = () => {
-        if (clientId) {
-            calls.fetchClients((clients) => {
-                const client = clients.find(c => c.id === clientId);
-                if (client) {
-                    setClientContactInfo({
-                        name: `${client.first_name} ${client.last_name}`,
-                        email: client.contact_email,
-                        phone: client.contact_phone,
-                    });
-                }
-            });
+  const fetchClientContactInfo = () => {
+    if (clientId) {
+      calls.fetchClients((clients) => {
+        const client = clients.find(c => c.id === clientId);
+        if (client) {
+          setClientContactInfo({
+            name: `${client.first_name} ${client.last_name}`,
+            email: client.contact_email,
+            phone: client.contact_phone,
+          });
         }
-    };
+      });
+    }
+  };
 
-    useEffect(() => {
-        fetchClientContactInfo();
-    }, [clientId]); 
+  useEffect(() => {
+    fetchClientContactInfo();
+  }, [clientId]);
 
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible);
-    };
+  return (
+    <View style={styles.contactWrapper}>
+      <Button title="Contact Info" onPress={() => setIsVisible(!isVisible)} />
 
-    return (
-        <View style={styles.container}>
-            <Button title="Contact Info" onPress={toggleVisibility} />
-            {isVisible && clientContactInfo && (
-                <View style={styles.detailsContainer}>
-                    <Text style={styles.detailText}>Name: {clientContactInfo.name}</Text>
-                    <Text style={styles.detailText}>Email: {clientContactInfo.email}</Text>
-                    <Text style={styles.detailText}>Phone: {clientContactInfo.phone}</Text>
-                </View>
-            )}
+      {isVisible && clientContactInfo && (
+        <View style={styles.detailsContainer}>
+          <Text style={styles.detailText}>Name: {clientContactInfo.name}</Text>
+          <Text style={styles.detailText}>Email: {clientContactInfo.email}</Text>
+          <Text style={styles.detailText}>Phone: {clientContactInfo.phone}</Text>
         </View>
-    );
+      )}
+    </View>
+  );
 };
 
 const DropdownSection = ({ onClientSelect, onLocationSelect }) => {
-    const [selectedClient, setSelectedClient] = useState(null);
-    const [clients, setClients] = useState([]);
-    const [locations, setLocations] = useState([]);
-    
+  const [selectedClient, setSelectedClient] = useState(null);
+  const [clients, setClients] = useState([]);
+  const [locations, setLocations] = useState([]);
 
-    useEffect(() => {
-        calls.fetchClients((clientData) => {
-            const formattedClients = clientData.map(client => ({
-                label: client.client_name,
-                value: client.id,
-                locations: client.locations
-            }));
-            setClients(formattedClients);
-        });
-    }, []);
+  useEffect(() => {
+    calls.fetchClients((clientData) => {
+      const formattedClients = clientData.map(client => ({
+        label: client.client_name,
+        value: client.id,
+        locations: client.locations
+      }));
+      setClients(formattedClients);
+    });
+  }, []);
 
-    const onClientValueChange = (clientId) => {
-        const id = parseInt(clientId);
-        const selectedClient = clients.find(client => client.value === id);
-        setSelectedClient(selectedClient);
-        if (selectedClient) {
-            const locationItems = selectedClient.locations.map(location => ({
-                label: location.name,
-                value: location.id,
-            }));
-            setLocations(locationItems);
-        } else {
-            setLocations([]);
-        }
-    };
-    const toggleNotes = () => {
-      setShowNotes(!showNotes);
+  const onClientValueChange = (clientId) => {
+    const id = parseInt(clientId);
+    const selected = clients.find(c => c.value === id);
+
+    setSelectedClient(selected);
+
+    if (selected) {
+      const locationItems = selected.locations.map(location => ({
+        label: location.name,
+        value: location.id,
+      }));
+      setLocations(locationItems);
+    } else {
+      setLocations([]);
+    }
   };
 
-    return (
-        <View style={styles.mainContainer}>
-            <View style={styles.dropdownSection}>
-                <View style={styles.dropdownContainer}>
-                    <Text style={styles.dropdownLabel}>Client:</Text>
-                    <RNPickerSelect
-                        onValueChange={(value) => {
-                          onClientSelect(value);
-                          onClientValueChange(value);
-                        }}
-                        items={clients}
-                        style={pickerSelectStyles}
-                        placeholder={{ label: "Select a client", value: null }}
-                    />
-                </View>
-                <View style={styles.dropdownContainer}>
-                    <Text style={styles.dropdownLabel}>Pickup Location:</Text>
-                    <RNPickerSelect
-                        onValueChange={(value) => {
-                          onLocationSelect(value);
-                        }}
-                        items={locations}
-                        style={pickerSelectStyles}
-                        placeholder={{ label: "Select a location", value: null }}
-                    />
-                </View>
-            </View>
-           
-            {selectedClient && <Contact style={styles.Contact}clientId={selectedClient.value} />}
-           
-            
-        </View>
-    );
+  return (
+    <View style={styles.mainContainer}>
+
+      {/* CLIENT DROPDOWN */}
+      <View style={styles.dropdownContainer}>
+        <Text style={styles.dropdownLabel}>Client</Text>
+        <RNPickerSelect
+          onValueChange={(value) => {
+            onClientSelect(value);
+            onClientValueChange(value);
+          }}
+          items={clients}
+          style={pickerSelectStyles}
+          placeholder={{ label: "Select a client", value: null }}
+          useNativeAndroidPickerStyle={false}
+        />
+      </View>
+
+      {/* LOCATION DROPDOWN */}
+      <View style={styles.dropdownContainer}>
+        <Text style={styles.dropdownLabel}>Pickup Location</Text>
+        <RNPickerSelect
+          onValueChange={onLocationSelect}
+          items={locations}
+          style={pickerSelectStyles}
+          placeholder={{ label: "Select a location", value: null }}
+          useNativeAndroidPickerStyle={false}
+        />
+      </View>
+
+      {/* CONTACT INFO */}
+      {selectedClient && (
+        <Contact clientId={selectedClient.value} />
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flexDirection: 'column', 
-       
-    },
+  mainContainer: {
+    padding: 16,
+    backgroundColor: "#F8FBFF",
+  },
 
-    Contact:{
-      alignItems:'center',
-    },
+  dropdownContainer: {
+    marginBottom: 20,
+  },
 
-    
-    dropdownSection: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    dropdownContainer: {
-        flexDirection: 'column',
-        width: '45%',
-    },
-    dropdownLabel: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        color: 'black',
-        alignItems:'center'
-    },
-   
+  dropdownLabel: {
+    fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 6,
+    color: "#0D47A1",
+  },
 
-    
+  contactWrapper: {
+    marginTop: 10,
+  },
+
+  detailsContainer: {
+    marginTop: 12,
+    padding: 12,
+    backgroundColor: "#E3F2FD",
+    borderRadius: 8,
+  },
+
+  detailText: {
+    fontSize: 15,
+    marginBottom: 5,
+    color: "#000",
+  },
 });
 
-
+// ★★★ Modern Beautiful Dropdown Styles ★★★
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
     fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 4,
-    color: 'black',
-    paddingRight: 30, 
+    paddingVertical: 14,
+    paddingHorizontal: 14,
+    borderWidth: 1.2,
+    borderColor: "#1976D2",
+    borderRadius: 10,
+    backgroundColor: "#F5F9FF",
+    color: "black",
+    paddingRight: 30,
+    marginTop: 3,
+
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 3,
   },
+
   inputAndroid: {
-    fontSize: 12,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: 'purple',
-    borderRadius: 8,
-    color: 'black',
-    paddingRight: 30, 
+    fontSize: 16,
+    paddingHorizontal: 14,
+    paddingVertical: 12,
+    borderWidth: 1.2,
+    borderColor: "#1976D2",
+    borderRadius: 10,
+    backgroundColor: "#F5F9FF",
+    color: "black",
+    paddingRight: 30,
+    marginTop: 3,
+    elevation: 2,
+  },
+
+  placeholder: {
+    color: "#78849E",
+    fontSize: 15,
   },
 });
 
